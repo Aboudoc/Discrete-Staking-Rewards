7fc0aa33120eb44e957aacc66c6dec862dc8ea9c
Aboudoc <aboutika.reda@gmail.com>
2023-02-18 20:00:16 +0100

    initial commit

---
+++ contracts/DiscreteStakingRewards.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./IERC20.sol";
+
+contract DiscreteStakingRewards {
+    IERC20 public immutable stakingToken;
+    IERC20 public immutable rewardToken;
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+
+    uint private rewardIndex;
+    mapping(address => uint) private rewardIndexOf;
+    mapping(address => uint) private earned;
+    uint private constant MULTIPLIER = 1e18;
+
+    constructor(address _stakingToken, address _rewardToken) {
+        stakingToken = IERC20(_stakingToken);
+        rewardToken = IERC20(_rewardToken);
+    }
+
+    function updateRewardIndex(uint reward) external {
+        rewardToken.transferFrom(msg.sender, address(this), reward);
+        rewardIndex += (reward * MULTIPLIER) / totalSupply;
+    }
+
+    function _calculateRewards(address account) private view returns (uint) {
+        uint shares = balanceOf[msg.sender];
+        return (shares * (rewardIndex - rewardIndexOf[account])) / MULTIPLIER;
+    }
+
+    function calculateRewardsEarned(address account)
+        external
+        view
+        returns (uint)
+    {
+        return earned[account] + _calculateRewards(account);
+    }
+
+    function _updateRewards(address account) private {
+        earned[account] += _calculateRewards(account);
+        rewardIndexOf[account] = rewardIndex;
+    }
+
+    function stake(uint amount) external {
+        _updateRewards(msg.sender);
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        stakingToken.transferFrom(msg.sender, address(this), amount);
+    }
+
+    function unstake(uint amount) external {
+        _updateRewards(msg.sender);
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        stakingToken.transfer(msg.sender, amount);
+    }
+
+    function claim() external returns (uint) {
+        _updateRewards(msg.sender);
+        uint reward = earned[msg.sender];
+        if (reward > 0) {
+            rewardToken.transfer(msg.sender, reward);
+            earned[msg.sender] = 0;
+        }
+
+        return reward;
+    }
+}
